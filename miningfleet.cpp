13b57074d102fc506169f45b55c1d5e611a291f7
ianlshaw <ian.shaw1991@gmail.com>
2024-03-24 13:40:03 +0200

    update CURLOPT_POSTFIELDS  to CURLOPT_COPYPOSTFIELDS when passing json through a function

--- miningfleet.cpp
+++ miningfleet.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <curl/curl.h>
-#include "json.hpp"
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
 #include <fstream>
+#include "spacetraders/nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 using namespace std;
-using json = nlohmann::json;
 
 string callsign;
 string targetResource;
@@ -25,6 +26,12 @@ namespace
         out->append(in, totalBytes);
         return totalBytes;
     }
+}
+
+void print_json(json jsonObject){
+    int indent = 4;
+    string pretty_json = jsonObject.dump(indent);
+    cout << pretty_json;
 }
 
 void populate_target_resource(string tradeSymbol){
@@ -61,7 +68,7 @@ string read_auth_token_from_file(const string authTokenFile){
 
 
 
-void naked_post(string endpoint, json payload = {}){
+void http_post(const string endpoint, const json payload = {}){
     // so we can see whats going on
     cout << "POST-ing to:" << endl;
     cout << endpoint << endl;
@@ -70,41 +77,62 @@ void naked_post(string endpoint, json payload = {}){
     CURL *curl;
     CURLcode res;
 
-     curl_global_init(CURL_GLOBAL_ALL);
+    curl_global_init(CURL_GLOBAL_ALL);
 
-     /* get a curl handle */
-     curl = curl_easy_init();
-     if(curl) {
-       curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
-       // convert payload recieved by parameter to a string
-       // construct the headers according to libcurl
-       struct curl_slist *headers = NULL;
-       headers = curl_slist_append(headers, "Content-Type: application/json");
-       string auth_token_header = "Authorization: Bearer " + read_auth_token_from_file(callsign + ".token");
+    /* get a curl handle */
+    curl = curl_easy_init();
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
+        //curl_easy_setopt(curl, CURLOPT_POST, 1L);
+        
+        /* ask libcurl to show us the verbose output */
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
-
-       headers = curl_slist_append(headers, auth_token_header.c_str());
-       curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+       // POSTFIELDS 
        if (payload.empty()){
+           cout << "payload is empty" << endl;
            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "");
        } else {
-           string payloadString = payload;
-           curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payloadString.c_str());
+           cout << "payload is not empty" << endl;
+           string payload_as_string = payload.dump();
+           cout << payload_as_string.c_str() << endl;
+           //long payloadLength = strlen(payload_as_string.c_str());
+           //cout << payloadLength << endl;
+           const char* payload_as_c_string = payload_as_string.c_str();
+           //curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload_as_c_string);
+           curl_easy_setopt(curl, CURLOPT_COPYPOSTFIELDS, payload_as_c_string);
+
+
+           //curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, payloadLength);
        }
-       curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, -1L);
-   
+
+       // construct the headers according to libcurl
+       struct curl_slist *headers = NULL;
+
+       // everybody needs a Content-Type header
+       headers = curl_slist_append(headers, "Content-Type: application/json");
+
+       // only set bearer if auth file exists. this is to support initial register agent
+       if (does_auth_file_exist(callsign + ".token")){
+           string auth_token_header = "Authorization: Bearer " + read_auth_token_from_file(callsign + ".token");
+           headers = curl_slist_append(headers, auth_token_header.c_str());
+       }
+
+       // provide curl handle its carefully crafted headers
+       curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+       // this will definitely become 20X 
        long httpCode(0);
+
        // prepare a container for returned data
        std::unique_ptr<std::string> httpData(new std::string());
+
        // Hook up data handling function.
        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
        curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
    
-   
        /* Perform the request, res gets the return code */
        res = curl_easy_perform(curl);
-
-       cout << httpCode;
 
        /* Check for curl level errors */
        if(res != CURLE_OK)
@@ -113,21 +141,30 @@ void naked_post(string endpoint, json payload = {}){
 
 
        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
-
+       
        // parse the response body into a json object.
        json output_as_json = json::parse(*httpData);
 
        // for now just print the result to stdout
-       int indent = 4;
-       string pretty_json = output_as_json.dump(indent);
-       cout << pretty_json;
+       print_json(output_as_json);
 
-          
+       // always reset curl handle after use   
        curl_easy_reset(curl);
+
+       // this wll be moved once we start reusing this function
        curl_easy_cleanup(curl);
+
      }
 }
 
+void write_auth_token_to_file(const string token){
+     ofstream myfile;
+     string filename = callsign + ".token";
+     cout << "[INFO] Wrote auth file to: " << filename << endl;
+     myfile.open (filename);
+     myfile << token;
+     myfile.close();
+}
 
 void register_agent() {
     cout << "Enter desired CALLSIGN:" << endl;
@@ -137,84 +174,17 @@ void register_agent() {
         return;
     }
 
-  cout << "[INFO] Attempting to register new agent " << callsign << endl;
-  CURL *curl;
-  CURLcode res;
- 
-  /* In windows, this inits the winsock stuff */
-  curl_global_init(CURL_GLOBAL_ALL);
- 
-  /* get a curl handle */
-  curl = curl_easy_init();
-  if(curl) {
-    /* First set the URL that is about to receive our POST. This URL can
-       just as well be an https:// URL if that is what should receive the
-       data. */
-    curl_easy_setopt(curl, CURLOPT_URL, "https://api.spacetraders.io/v2/register");
-    /* Now specify the POST data */
-
-  
+    cout << "[INFO] Attempting to register new agent " << callsign << endl;
     cout << "Enter faction name" << endl;
     string faction;
     cin >> faction;
 
-    json payload;
-    payload["faction"] = faction;
-    payload["symbol"] = callsign;
+    json register_agent_json_object = {};
+    register_agent_json_object["symbol"] = callsign;
+    register_agent_json_object["faction"] = faction;
+    http_post("https://api.spacetraders.io/v2/register", register_agent_json_object);
+    //json result = http_post("https://api.spacetraders.io/v2/register", payload);
 
-    string payload_as_string = payload.dump();
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload_as_string.c_str());
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "Content-Type: application/json");
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, -1L);
-
-    long httpCode(0);
-    std::unique_ptr<std::string> httpData(new std::string());
-    // Hook up data handling function.
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
-
- 
-    /* Perform the request, res gets the return code */
-    res = curl_easy_perform(curl);
-    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
-
-
-    if (httpCode != 201) {
-      cout << "!!! ERROR !!!" << endl;
-      cout << "HTTP RETURN CODE:" << endl;
-      cout << httpCode << endl;
-      cout << "HTTP BODY:" << endl;
-      json output_as_json = json::parse(*httpData);
-      int indent = 4;
-      string pretty_json = output_as_json.dump(indent);
-      cout << pretty_json;
-      exit(1);
-    }
-
-    // parse the response body into a json object.
-    json output_as_json = json::parse(*httpData);
-
-    // this is how to selectively return parts of the json object.
-    cout << "[INFO] Callsign registration succeeded " << endl;
-    string token = output_as_json["data"]["token"];
-    ofstream myfile;
-    string filename = callsign + ".token";
-    cout << "[INFO] Wrote auth file to: " << filename << endl;
-    myfile.open (filename);
-    myfile << token;
-    myfile.close();
-
-
-    /* Check for errors */
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    cout << "curl easy reset" << endl;
-    curl_easy_reset(curl);
-  }
 }
 
 void populate_contract_globals(){
@@ -296,15 +266,29 @@ void populate_contract_globals(){
 void acceptContract(const string contractId) {
     cout << "attempting to accept contract " + contractId << endl;
     string endpoint = "https://api.spacetraders.io/v2/my/contracts/" + contractId + "/accept";
-    naked_post(endpoint);
+    http_post(endpoint);
+    //print_json(result);
 }
 
 
-// find target resource
+void useful_but_not_yet(){
+//    if (httpCode != 201) {
+//      cout << "!!! ERROR !!!" << endl;
+//      cout << "HTTP RETURN CODE:" << endl;
+//      cout << httpCode << endl;
+//      cout << "HTTP BODY:" << endl;
+//      json output_as_json = json::parse(*httpData);
+//      int indent = 4;
+//      string pretty_json = output_as_json.dump(indent);
+//      cout << pretty_json;
+//      exit(1);
+//    }
+}
  
 int main(void)
 {
   register_agent();
+  return 0;
   populate_contract_globals();
   acceptContract(targetContractId);
   curl_global_cleanup();
